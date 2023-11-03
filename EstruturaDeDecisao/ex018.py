924f62a9964a1ca8f8a967e0811e164a6ce8f517
Natan Xavier <55638524+natan-xav2019@users.noreply.github.com>
2023-11-02 22:53:16 -0300

    Validador de data formato "dd/mm/aaaa"

--- EstruturaDeDecisao/ex018.py
+++ EstruturaDeDecisao/ex018.py
@@ -1,4 +1,52 @@
 # Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
+import re
 
-data = input("Digite a data no formato \"dd/mm/aaaa\" por favor para ser considerada valida: ")
+dia_cada_mes = [31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+def valida_data(data) -> bool:
+    dia = int(data[0])
+    mes = int(data[1])
+    ano = int(data[2])
+    
+    limite_ano = ano > 0
+    limite_mes = 0 < mes <= 12
+    if(limite_ano):
+        ano_bissexto = ano % 4 == 0
+    else:
+        return False
+    
+    print(f"ano_bissexto: {ano_bissexto}")
+
+    if(ano_bissexto):
+        dia_cada_mes[1] = 29
+    else:
+        dia_cada_mes[1] = 28
+
+
+    if(limite_mes):
+        if 0 < dia <= dia_cada_mes[mes-1]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+padrao = re.compile(r"^\d{2}\/\d{2}\/\d{4}$")
+
+while True:
+    texto = input("Digite a data no formato \"dd/mm/aaaa\" por favor para ser considerada valida: ")
+    data = re.fullmatch(padrao, texto)
+    if not (data == None) :
+        dataArray = data.group().split("/")
+        
+        aprovado = valida_data(dataArray)
+        
+        if(aprovado):
+            break
+        else:
+            print(f"A Data não esta dentro dos padrões.")
+    else:
+        print(f"Digite Apenas data no formato \"dd/mm/aaaa\" .")
+            
+
+print(f"Data valida: {dataArray[0]}/{dataArray[1]}/{dataArray[2]}")
