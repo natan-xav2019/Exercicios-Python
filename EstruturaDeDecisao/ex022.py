620d1bd8f4f028a61f48f61f5ba55f2a98f29241
Natan Xavier <natanxaviersiquera@gmail.com>
2023-11-28 06:24:19 -0300

    Número inteiro ou decimal
    # Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de arredondamento.

---
+++ EstruturaDeDecisao/ex022.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+
+numero = int(input("digite um numero: "))
+
+if(numero % 2 == 0):
+    print(f"O numero {numero} e par!")
+else:
+    print(f"O numero {numero} e Impar!")
