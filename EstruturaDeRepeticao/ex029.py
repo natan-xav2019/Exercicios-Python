cc42086c56d4c1fc77cb215978106f235b7b502b
Natan Xavier <natanxaviersiquera@gmail.com>
2023-12-26 19:28:33 -0300

    Tabela de preços 1.99
    # O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca de 10 caixas. Para agilizar o cálculo de quanto cada cliente deve pagar ele desenvolveu um tabela que contém o número de itens que o cliente comprou e ao lado o valor da conta. Desta forma a atendente do caixa precisa apenas contar quantos itens o cliente está levando e olhar na tabela de preços. Você foi contratado para desenvolver o programa que monta esta tabela de preços, que conterá os preços de 1 até 50 produtos, conforme o exemplo abaixo:
    # Lojas Quase Dois - Tabela de preços
    # 1 - R$ 1.99
    # 2 - R$ 3.98
    # ...
    # 50 - R$ 99.50

---
+++ EstruturaDeRepeticao/ex029.py
@@ -0,0 +1,21 @@
+# O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca de 10 caixas. Para agilizar o cálculo de quanto cada cliente deve pagar ele desenvolveu um tabela que contém o número de itens que o cliente comprou e ao lado o valor da conta. Desta forma a atendente do caixa precisa apenas contar quantos itens o cliente está levando e olhar na tabela de preços. Você foi contratado para desenvolver o programa que monta esta tabela de preços, que conterá os preços de 1 até 50 produtos, conforme o exemplo abaixo:
+# Lojas Quase Dois - Tabela de preços
+# 1 - R$ 1.99
+# 2 - R$ 3.98
+# ...
+# 50 - R$ 99.50
+
+import locale
+
+locale.setlocale(locale.LC_ALL,"pt_BR")
+
+VALOR_PRODUTO = 1.99
+QUANTIDADE_PRODUTOS = 50
+
+print("Lojas Quase Dois - Tabela de preços")
+
+for contador in range(1,QUANTIDADE_PRODUTOS + 1):
+    
+    preso = VALOR_PRODUTO * contador
+
+    print(f"{contador:2d} - {locale.currency(preso)}")
