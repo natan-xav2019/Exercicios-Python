f3df5a2510ec73c6d39f04567dd7fca8ce2ce2b1
Natan Xavier <natanxaviersiquera@gmail.com>
2024-01-21 15:11:02 -0300

    Verificar maior, menor, quantidade, média
    # Desenvolver um programa para verificar a nota do aluno em uma prova com 10 questões, o programa deve perguntar ao aluno a resposta de cada questão e ao final comparar com o gabarito da prova e assim calcular o total de acertos e a nota (atribuir 1 ponto por resposta certa). Após cada aluno utilizar o sistema deve ser feita uma pergunta se outro aluno vai utilizar o sistema. Após todos os alunos terem respondido informar:
    # Maior e Menor Acerto;
    # Total de Alunos que utilizaram o sistema;
    # A Média das Notas da Turma.
    # Gabarito da Prova:

---
+++ EstruturaDeRepeticao/ex045.py
@@ -0,0 +1,77 @@
+# Desenvolver um programa para verificar a nota do aluno em uma prova com 10 questões, o programa deve perguntar ao aluno a resposta de cada questão e ao final comparar com o gabarito da prova e assim calcular o total de acertos e a nota (atribuir 1 ponto por resposta certa). Após cada aluno utilizar o sistema deve ser feita uma pergunta se outro aluno vai utilizar o sistema. Após todos os alunos terem respondido informar:
+# Maior e Menor Acerto;
+# Total de Alunos que utilizaram o sistema;
+# A Média das Notas da Turma.
+# Gabarito da Prova:
+
+QUANTIDADE_NOTAS = 10
+OPISOES_NOTA = ["A","B","C","D","E"]
+GABARITO = ["A","B","C","D","B","B","C","D","E","A"]
+
+class Estudante:
+    def __init__(self,respostas:list[str]) -> None:
+        self.respostas = respostas
+        self.nota = 0
+        self.resultado()
+
+    def resultado(self):
+        for indice,questao in enumerate(self.respostas):
+            if GABARITO[indice] == questao:
+                self.nota += 1
+        
+alunos:list[Estudante] = []
+
+questao = []
+denovo = False
+continuar = True
+contador = 0
+
+while continuar:
+    try:
+        
+        while contador < 10:
+            if denovo:
+                questao[contador] = input(f"Digite a nota {contador+1} ").upper()
+            else:
+                denovo = True
+                questao.append(input(f"Digite a nota {contador+1} ").upper())
+            if questao[contador] in OPISOES_NOTA:
+                contador += 1
+                denovo = False
+            else:
+                print(f"Digite apenas as opções {OPISOES_NOTA}") 
+
+    except:
+        print(f"Digite apenas as opções {OPISOES_NOTA}")
+
+    alunos.append(Estudante(questao))
+    questao.clear()
+    contador = 0
+    while True:
+        mais_aluno = input("Deseja continuar? ").upper()
+        if mais_aluno in "S":
+            continuar = True
+            break
+        elif mais_aluno in "N":
+            continuar = False
+            break
+        else:
+            print("Digite apenas S ou N.")
+
+maior = 0
+menor = 0
+total_notas = 0
+
+for indice,aluno in enumerate(alunos):
+    if alunos[maior].nota < aluno.nota:
+        maior = indice
+    if alunos[menor].nota > aluno.nota:
+        menor = indice
+    total_notas += aluno.nota
+
+nota_media = total_notas/QUANTIDADE_NOTAS
+
+print(f"Maior nota: {alunos[maior].nota}")
+print(f"Menor nota: {alunos[menor].nota}")
+print(f"quantidade de alunos {len(alunos)}")
+print(f"Gabarito: {GABARITO}")
