526da5c18e3c59ad2ec862b3bff4bef82e169812
Natan Xavier <natanxaviersiquera@gmail.com>
2024-01-23 14:48:34 -0300

    leia um vetor de 5 números inteiros e mostre-os
    # Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.

---
+++ ExerciciosListas/ex001.py
@@ -0,0 +1,8 @@
+# Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+
+vetor = []
+
+for contador in range(0,5):
+    vetor.append(int(input("Digite um numero inteiro: ")))
+
+print(vetor)
