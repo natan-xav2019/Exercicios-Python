d3ff1dc78bd78f854b04114d16abbac1691d79e0
Natan Xavier <natanxaviersiquera@gmail.com>
2024-02-18 21:24:20 -0300

    Leia 10 letras e diga quantas consoantes
    # Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.

--- ExerciciosListas/ex004.py
+++ ExerciciosListas/ex004.py
@@ -1,31 +1,29 @@
-# Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
+# Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
 
-TAMANHO = 20
+def quantidadeConsoante( letras:list[str] ):
+    total = 0
+    for letra in letras:
+        if not letra.upper() in ["A","E","I","O","U"]:
+            total = total + 1
 
-vetor = []
-par = []
-impar = []
+    return total
+
+TAMANHO = 10
+letras = []
 contador = 0
 
 while contador < TAMANHO:
     try:
-        numero = int(input(f"{contador+1} Digite o numero: "))
-        vetor.append(numero)
-
-        if numero % 2 == 0:
-            par.append(numero)
+        letra = input(f"{ contador + 1 } Digite a letra: ")
+        
+        if len(letra) == 1 and letra.isalpha() :
+            letras.append( letra )
+            contador += 1
         else:
-            impar.append(numero)
-
-        contador += 1
+            print("Apenas uma letra.")
     except:
-        print("Digite apenas numeros inteiros.")
+        print("Digite apenas uma letra.")
 
-print("Vetor")
-print(vetor)
+consoante = quantidadeConsoante(letras)
 
-print("Vetor Par")
-print(par)
-
-print("Vetor Impar")
-print(impar)
+print(consoante)
